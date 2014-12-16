6507281640e4fa8d8cd50407973c3eba5c193302
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-16 01:36:48 -0500

    Add Guard minitests and backtrace_silencers

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -2,12 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  reporter_options = { color: true, slow_count: 5 }
-
-  Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 end
