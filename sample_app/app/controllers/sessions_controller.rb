5f5e313b5eb3af382861c0478f254c9a7ba32d03
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-17 16:20:28 -0500

    Add newly created log_in method to create, that way the user is logged in and redirected to his profile page upon session creation

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,12 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+  		# find the user by it's email in the db
   	user = User.find_by(email: params[:session][:email].downcase)
+  		# if the user was found and the password was correct...
   	if user && user.authenticate(params[:session][:password])
-  		# Log the user in and redirect to user's show page
+  		# Log the user in and 
+  		log_in user
+  		# redirect to user's show page
+  		redirect_to user
   	else
   		# Create an error message
+  		# the method 'now' avoids the persistance of the message
   		flash.now[:danger] = 'Invalid email/password combination'
+  		# Display the login form again
   		render 'new'
   	end
   end
