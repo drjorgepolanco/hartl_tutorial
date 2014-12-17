e745b5851b58ff8b921a97f9bdac0eefd0b66ba3
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-17 16:07:25 -0500

    Add integration test to avoid persistence of flash messages in login

---
+++ sample_app/test/integration/users_login_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+	test "login with invalid information" do
+		# Visit the login path
+		get login_path																				
+		# Verify that the new sessions form renders properly
+		assert_template 'sessions/new'												
+		# Post to the sessions path with an invalid params hash
+		post login_path, session: { email: "", password: "" }
+		# Verify that new sessions form gets re-rendered and 
+		assert_template 'sessions/new'
+		# that flash message appears
+		assert_not flash.empty?
+		# Visit another page (such as the Home page)
+		get root_path
+		# Verify that the flash message doesn’t appear on the new page
+		assert flash.empty?
+	end
+end
