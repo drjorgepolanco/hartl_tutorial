7c93b9a294132c82c3252f1185c59a3c07392b9d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-16 21:20:39 -0500

    Improve test to considerate all upcase emails

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -45,8 +45,9 @@ class UserTest < ActiveSupport::TestCase
 		end
 	end
 
-	test "email address should be unique" do
+	test "email addresses should be unique" do
 		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
 		@user.save
 		assert_not duplicate_user.valid?
 	end
