eb351951e2130926c4f23e49466a2ccc8a2b6d66
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-19 02:43:13 -0500

    Add method correct_user to allow a user updating another user's profile

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
-  
+  before_action :correct_user,   only: [:edit, :update] 
+
   def show
   	@user = User.find(params[:id])
   end
@@ -40,7 +41,7 @@ class UsersController < ApplicationController
 	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
 
-    # Before filters
+  # Before filters
 
     # Confirms a logged-in user
     def logged_in_user
@@ -48,5 +49,11 @@ class UsersController < ApplicationController
         flash[:danger] = "Please, log in."
         redirect_to login_url
       end
+    end
+
+    # Confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
