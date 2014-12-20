6c4674938449cb4c9023a8d66af2b951edd44c94
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-20 01:31:18 -0500

    should redirect index when not logged in

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionController::TestCase
 		@user = users(:jorge)
 		@other_user = users(:triculito)
 	end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
   
   test "should get new" do
     get :new
