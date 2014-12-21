631fc386226a961f5692ab61a3e2c709225176c6
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-21 16:59:44 -0500

    Create private methods downcase_email which is called before_save and create_activation_digest which is called before_create

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
-	before_save { email.downcase! }
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email
+	before_create :create_activation_digest
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
@@ -8,7 +9,6 @@ class User < ActiveRecord::Base
 										uniqueness: { case_sensitive: false }
 	has_secure_password												 # allows edit without password ->
 	validates :password, length: { minimum: 6 }, allow_blank: true # <- <- <- <-
-
 
 # METHODS FOR REMEMBER USER
 
@@ -44,4 +44,19 @@ class User < ActiveRecord::Base
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+# METHODS FOR ACCOUNT ACTIVATION
+
+	private
+
+		# Converts email to all lower-case
+		def downcase_email
+			self.email = email.downcase
+		end
+
+		# Creates and assigns the activation token and digest
+		def create_activation_digest
+			self.activation_token  = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 end
