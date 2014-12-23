a214f11bb443a1fd9007722631a3a5b943ebd877
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-23 01:50:46 -0500

    Update password_reset

--- sample_app/app/mailers/user_mailer.rb
+++ sample_app/app/mailers/user_mailer.rb
@@ -6,9 +6,8 @@ class UserMailer < ApplicationMailer
   end
 
   def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  	@user = user
+  	mail to: user.email, subject: "Password reset"
   end
   
 end
