374ca8fb6170a380e03113635d3ab75e6e25ecf3
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-23 01:48:17 -0500

    Create methods: create_reset_digest and send_password_reset_email

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest
 	validates :name,  presence: true, length: { maximum: 50 }
@@ -45,6 +45,7 @@ class User < ActiveRecord::Base
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
 # METHODS FOR USER ACTIVATION
 
 	# Activates an account
@@ -58,7 +59,19 @@ class User < ActiveRecord::Base
 		UserMailer.account_activation(self).deliver_now
 	end
 
-# METHODS FOR ACCOUNT ACTIVATION
+# METHODS FOR RESET TOKEN
+
+	# Sets the password reset attributes
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	# Sends password reset email
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
 
 	private
 
@@ -66,6 +79,8 @@ class User < ActiveRecord::Base
 		def downcase_email
 			self.email = email.downcase
 		end
+
+	# METHOD FOR ACCOUNT ACTIVATION
 
 		# Creates and assigns the activation token and digest
 		def create_activation_digest
