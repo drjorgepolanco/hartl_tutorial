761bfd7a9cdfcd815d69bf7bfc6fb6f3bb6543c5
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-25 18:34:24 -0500

    Define method feed

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -80,6 +80,13 @@ class User < ActiveRecord::Base
 		reset_sent_at < 2.hours.ago
 	end
 
+# METHOD FOR MICROPOSTS FEED
+	
+	# Defines a proto-feed
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
+
 	private
 
 		# Converts email to all lower-case
