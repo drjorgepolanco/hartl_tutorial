1b3ba59e40898cde9eecabf50d686d2a0325db9f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-25 22:28:46 -0500

    Install ImageMagick and add size limit restriction

--- sample_app/app/uploaders/picture_uploader.rb
+++ sample_app/app/uploaders/picture_uploader.rb
@@ -1,13 +1,11 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
